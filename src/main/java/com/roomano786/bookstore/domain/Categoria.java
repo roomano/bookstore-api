036c1b855e58fdad0f3e53ad5b9fca2609b26ff1
roomano <romanoivanpedro@gmail.com>
2021-06-04 03:33:16 +0200

    classes de domínio iniciadas

---
+++ src/main/java/com/roomano786/bookstore/domain/Categoria.java
@@ -0,0 +1,90 @@
+package com.roomano786.bookstore.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Categoria {
+    private Integer id;
+    private String nome;
+    private String descricao;
+
+    private List<Livro> livros = new ArrayList<>();
+
+    public Categoria() {
+        super();
+    }
+
+    public Categoria(Integer id, String nome, String descricao) {
+        super();
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(List<Livro> livros) {
+        this.livros = livros;
+    }
+/*
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Categoria)) return false;
+        Categoria categoria = (Categoria) o;
+        return getId().equals(categoria.getId());
+    }
+    */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null)
+            return false;
+        if (getClass() != o.getClass())
+            return false;
+        Categoria other = (Categoria) o;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+   }
+
+    @Override
+    public int hashCode() {
+        //return Objects.hash(getId());
+        final int prime = 31;
+        int result =1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+}
---
+++ src/main/java/com/roomano786/bookstore/domain/Livro.java
@@ -0,0 +1,90 @@
+package com.roomano786.bookstore.domain;
+
+public class Livro {
+
+    private Integer id;
+    private String titulo;
+    private String nome_autor;
+    private String texto;
+
+    private Categoria categoria;
+
+    public Livro() {
+        super();
+    }
+
+    public Livro(Integer id, String titulo, String nome_autor, String texto, Categoria categoria) {
+        this.id = id;
+        this.titulo = titulo;
+        this.nome_autor = nome_autor;
+        this.texto = texto;
+        this.categoria = categoria;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getNome_autor() {
+        return nome_autor;
+    }
+
+    public void setNome_autor(String nome_autor) {
+        this.nome_autor = nome_autor;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null)
+            return false;
+        if (getClass() != o.getClass())
+            return false;
+        Livro other = (Livro) o;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        //return Objects.hash(getId());
+        final int prime = 31;
+        int result =1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+}
