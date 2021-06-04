d160407985fb99b9ec0c787f367c11f08d0eb5b5
roomano <romanoivanpedro@gmail.com>
2021-06-04 04:00:44 +0200

    criadas as interfaces repositories

---
+++ src/main/java/com/roomano786/bookstore/repositories/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.roomano786.bookstore.repositories;
+
+import com.roomano786.bookstore.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
---
+++ src/main/java/com/roomano786/bookstore/repositories/LivroRepository.java
@@ -0,0 +1,10 @@
+package com.roomano786.bookstore.repositories;
+
+import com.roomano786.bookstore.domain.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
+    
+}
