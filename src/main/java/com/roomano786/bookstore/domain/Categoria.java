8352d63db2fa11995ba9735a05de688d8317ff68
roomano <romanoivanpedro@gmail.com>
2021-06-04 03:47:05 +0200

    finalizando as classes de domínio

--- src/main/java/com/roomano786/bookstore/domain/Categoria.java
+++ src/main/java/com/roomano786/bookstore/domain/Categoria.java
@@ -1,14 +1,22 @@
 package com.roomano786.bookstore.domain;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class Categoria {
+@Entity
+public class Categoria implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String descricao;
 
+    @OneToMany(mappedBy = "categoria")
     private List<Livro> livros = new ArrayList<>();
 
     public Categoria() {
--- src/main/java/com/roomano786/bookstore/domain/Livro.java
+++ src/main/java/com/roomano786/bookstore/domain/Livro.java
@@ -1,12 +1,21 @@
 package com.roomano786.bookstore.domain;
 
-public class Livro {
+import javax.persistence.*;
+import java.io.Serializable;
 
+@Entity
+public class Livro implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String titulo;
     private String nome_autor;
     private String texto;
 
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
     private Categoria categoria;
 
     public Livro() {
